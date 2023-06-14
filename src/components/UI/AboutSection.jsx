1898143ea520e3fc997ec986b5a48252993b50c5
Arshia Naveed <arshianaveed884@gmail.com>
2023-06-14 23:58:25 +0500

    Footer and Donate Now Section

--- src/components/UI/AboutSection.jsx
+++ src/components/UI/AboutSection.jsx
@@ -8,44 +8,47 @@ import "@fontsource/poppins";
 const AboutSection = () => {
   return (
 
-    <div className="aboutsection">
+    
+    <div className="abt_sect">
+      <div className="aboutsection">
 
-      <div className="heading">
-        <p className="line"></p>
-        <p className="heading1">ABOUT US</p>
+<div className="heading">
+  <p className="line"></p>
+  <p className="heading1">ABOUT US</p>
+</div>
+
+<Container className="allcontent">
+  
+  <Row>
+    <Col sm={4}>
+      <div className="about__img">
+        <img src={aboutImg} alt="" className="w-100" />
       </div>
+    </Col>
 
-      <Container className="allcontent">
-        
-        <Row>
-          <Col sm={4}>
-            <div className="about__img">
-              <img src={aboutImg} alt="" className="w-100" />
-            </div>
-          </Col>
+    <Col sm={8}>
+      <p className="contentHeading">Lorem Ipsum</p>
+      <p className="para">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo nulla voluptatibus dolorem accusantium exercitationem sed animi corporis numquam. Ipsam numquam blanditiis commodi dolorem autem rem voluptatem cupiditate, quaerat expedita quasi!</p>
+    </Col>
+  </Row>
 
-          <Col sm={8}>
-            <p className="contentHeading">Lorem Ipsum</p>
-            <p className="para">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo nulla voluptatibus dolorem accusantium exercitationem sed animi corporis numquam. Ipsam numquam blanditiis commodi dolorem autem rem voluptatem cupiditate, quaerat expedita quasi!</p>
-          </Col>
-        </Row>
+  <Row id="contenttwo">
+  <Col sm={8}>
+      <p className="contentHeading">Lorem Ipsum</p>
+      <p className="para">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo nulla voluptatibus dolorem accusantium exercitationem sed animi corporis numquam. Ipsam numquam blanditiis commodi dolorem autem rem voluptatem cupiditate, quaerat expedita quasi!</p>
+    </Col>
 
-        <Row id="contenttwo">
-        <Col sm={8}>
-            <p className="contentHeading">Lorem Ipsum</p>
-            <p className="para">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo nulla voluptatibus dolorem accusantium exercitationem sed animi corporis numquam. Ipsam numquam blanditiis commodi dolorem autem rem voluptatem cupiditate, quaerat expedita quasi!</p>
-          </Col>
+    <Col sm={4}>
+      <div className="about__img">
+        <img src={aboutImg} alt="" className="w-100" />
+      </div>
+    </Col>
+  </Row>
 
-          <Col sm={4}>
-            <div className="about__img">
-              <img src={aboutImg} alt="" className="w-100" />
-            </div>
-          </Col>
-        </Row>
-
-      </Container>
+</Container>
+</div>
     </div>
 
   );
--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import AboutSection from "../components/UI/AboutSection";
 import Footer from "../components/Footer/footer";
 import OurGoals from "../components/OurGoals";
+import DonateNowSection from "../components/UI/DonateNowSection";
 
 
 const landingpage = () => {
   return (
     <div>
-    <OurGoals />
-    {/* =========== about section ================ */}
-       <AboutSection />
-    {/* =========== footer section ================ */}
-       <Footer />
+      <OurGoals />
+      {/* =========== about section ================ */}
+      <AboutSection />
+      {/* =========== Donate Now section ================ */}
+      <DonateNowSection/>
+      {/* =========== footer section ================ */}
+      <Footer />
     </div>
-    
+
   );
 };
 export default landingpage;
