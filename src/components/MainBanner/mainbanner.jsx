a60aac5e585f58864d66b1aa48dd8c8846ad8969
hasnainAhmed98 <62600978+hasnainAhmed98@users.noreply.github.com>
2023-07-16 21:12:33 +0500

    haeder and banner
    header and banner

--- src/components/MainBanner/mainbanner.jsx
+++ src/components/MainBanner/mainbanner.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import "../../styles/MainBanner.css"
+
 
 export default function mainbanner() {
   return (
     <>
-     adasd
+    <section className='Main-banner'>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-12 col-md-9 col-lg-8 col-xl-8 col-xxl-8">
+              <div className="content-box">
+                <h2>Let’s Build a Great future for others together</h2>
+                <p className='para'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet maximus elementum. Praesent eu mi euismod neque commodo volutpat. Suspendisse id ipsum vel mauris consectetur.</p>
+                <ul className='banner-btn'>
+                  <li>
+                    <a className='donate-btn' href="javascript:;">Donate Now</a>
+                  </li>
+                  <li>
+                    <a className='video-btn' href="javascript:;">Watch Video</a>
+                  </li>
+                </ul>
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
     </>
   )
 }
